91fde3d93d486d2fff71a14ef190e4f4c253fa57
Gal Ohayon <galohayon@Gals-MacBook-Air.local>
2025-11-04 23:16:59 +0200

    finish hw

--- Bill3.java
+++ Bill3.java
@@ -9,6 +9,6 @@ public class Bill3 {
 
         double cost = Math.ceil(sum / 3.0);
 
-        System.out.println("Dear " + name3 + ", " + name2 + " and " + name1 +": pay " + cost + " Shekels each");
+        System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 +": pay " + cost + " Shekels each");
     }
 }
